4bfa582643b93ed5aaeec080348fe12ac85e3e4b
Nguyễn Tiến Thành (164581) <ntt@git.vuivui.com>
2021-10-22 08:20:25 +0700

    [Edit] init cript database

--- DB/onlineauction_db.sql
+++ DB/onlineauction_db.sql
@@ -1,62 +1,164 @@
--- phpMyAdmin SQL Dump
--- version 4.9.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Sep 26, 2021 at 01:05 PM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.3.12
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+ Source Server         : online
+ Source Server Type    : MySQL
+ Source Server Version : 80027
+ Source Host           : localhost:3306
+ Source Schema         : online
 
+ Target Server Type    : MySQL
+ Target Server Version : 80027
+ File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ Date: 22/10/2021 08:18:08
+*/
 
---
--- Database: `onlineauction_db`
---
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for brand
+-- ----------------------------
+DROP TABLE IF EXISTS `brand`;
+CREATE TABLE `brand`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `is_deleted` bit(64) NULL DEFAULT NULL,
+  `cateted_at` datetime(0) NULL DEFAULT NULL,
+  `updated_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
---
--- Table structure for table `users`
---
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `is_deleted` bit(64) NULL DEFAULT NULL,
+  `cateted_at` datetime(0) NULL DEFAULT NULL,
+  `updated_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
-  `fullname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `address` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
-  `roles_id` int(11) NOT NULL,
-  `islock` tinyint(1) NOT NULL,
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`user_id`),
-  KEY `user_role` (`roles_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+-- ----------------------------
+-- Table structure for product
+-- ----------------------------
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `is_deleted` bit(64) NULL DEFAULT NULL,
+  `cateted_at` datetime(0) NOT NULL,
+  `updated_at` datetime(0) NOT NULL,
+  `ratting` int NULL DEFAULT NULL,
+  `time_start` datetime(0) NULL DEFAULT NULL,
+  `time_end` datetime(0) NULL DEFAULT NULL,
+  `price` decimal(10, 2) NULL DEFAULT NULL,
+  `brand_id` int NULL DEFAULT NULL,
+  `brand_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `category_id` int NULL DEFAULT NULL,
+  `category_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `timestamp` datetime(0) NULL DEFAULT NULL,
+  `avatar` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `images` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `current_price` decimal(10, 2) NULL DEFAULT NULL,
+  `max_price` decimal(10, 2) NULL DEFAULT NULL,
+  `count_quantity_bidder` int NULL DEFAULT NULL,
+  `seller_id` int NULL DEFAULT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `step` int NULL DEFAULT NULL,
+  `is_automatic` bit(64) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for product_bidder
+-- ----------------------------
+DROP TABLE IF EXISTS `product_bidder`;
+CREATE TABLE `product_bidder`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `bidder_id` int NULL DEFAULT NULL,
+  `cateted_at` datetime(0) NOT NULL,
+  `updated_at` datetime(0) NOT NULL,
+  `status` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
---
--- Table structure for table `user_roles`
---
+-- ----------------------------
+-- Table structure for transform_seller
+-- ----------------------------
+DROP TABLE IF EXISTS `transform_seller`;
+CREATE TABLE `transform_seller`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `bidder_id` int NULL DEFAULT NULL,
+  `bidder_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `cateted_at` datetime(0) NOT NULL,
+  `updated_at` datetime(0) NOT NULL,
+  `product_id` int NULL DEFAULT NULL,
+  `price` decimal(10, 2) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Table structure for user_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `user_comment`;
+CREATE TABLE `user_comment`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `bidder_id` int NULL DEFAULT NULL,
+  `bidder_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `cateted_at` datetime(0) NOT NULL,
+  `updated_at` datetime(0) NOT NULL,
+  `product_id` int NULL DEFAULT NULL,
+  `price` decimal(10, 2) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user_roles
+-- ----------------------------
 DROP TABLE IF EXISTS `user_roles`;
-CREATE TABLE IF NOT EXISTS `user_roles` (
-  `user_role_id` int(11) NOT NULL AUTO_INCREMENT,
-  `role_id` int(11) NOT NULL,
-  `role_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`user_role_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-COMMIT;
+CREATE TABLE `user_roles`  (
+  `user_role_id` int NOT NULL AUTO_INCREMENT,
+  `role_id` int NOT NULL,
+  `role_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`user_role_id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `fullname` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `roles_id` int NOT NULL,
+  `islock` tinyint(1) NOT NULL,
+  `ratting` int NOT NULL,
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`user_id`) USING BTREE,
+  INDEX `user_role`(`roles_id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for watch_list
+-- ----------------------------
+DROP TABLE IF EXISTS `watch_list`;
+CREATE TABLE `watch_list`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `bidder_id` int NULL DEFAULT NULL,
+  `bidder_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `cateted_at` datetime(0) NOT NULL,
+  `updated_at` datetime(0) NOT NULL,
+  `product_id` int NULL DEFAULT NULL,
+  `price` decimal(10, 2) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
