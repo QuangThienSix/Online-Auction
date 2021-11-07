4267d39aadc3dc4e9f624b6b28956074738dfa8f
Nguyễn Tiến Thành (164581) <ntt@git.vuivui.com>
2021-11-07 13:31:25 +0700

    [edit] api upload file

--- BE/src/controllers/uploadController.js
+++ BE/src/controllers/uploadController.js
@@ -13,68 +13,72 @@ import bcrypt from "bcrypt";
 import appConfig from "../config/env/app.dev.json";
 import rn from "random-number";
 import apiConfig from "../config/api";
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
+var express = require('express');
+var multer  = require('multer');
+var fs  = require('fs');
+
 var options = {
   // example input , yes negative values do work
   min: 1000,
   max: 9999,
 };
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+      var dir = 'src/uploads';
+      if (!fs.existsSync(dir)){
+          fs.mkdirSync(dir);
+      }
+      callback(null, dir);
+  },
+  filename: function (req, file, callback) {
+      callback(null, file.originalname);
+  }
+});
+var upload = multer({storage: storage}).array('files', 12);
+
 class UploadController extends BaseController {
   constructor() {
     super();
 
     this.single = this.single.bind(this);
+    this.getFile = this.getFile.bind(this);
     //  this.multiple = this.multiple.bind(this);
   }
-
   async single(req, res) {
     logger.info("uploadfile");
-  
-    var form = new formidable.IncomingForm();
-    //Thiết lập thư mục chứa file trên server
-    form.uploadDir = "uploads/";
-    //xử lý upload
-    form.parse(req, function (err, fields, file) {
-        //path tmp trên server
-        var path = file.files.path;
-        //thiết lập path mới cho file
-        var newpath = form.uploadDir + file.files.name;
-        fs.rename(path, newpath, function (err) {
-            if (err) throw err;
-            res.end('Upload Thanh cong!');
+    const { accessToken ,files} = req.body;
+
+    //const parseToken = deCodeTokenForUser(accessToken);
+    //if (parseToken) 
+    {
+      var images = [];
+    var data = await upload(req,res,function(err,result) {
+        console.log(err);
+            if(err) {
+                return res.end("Error uploading file."+err);
+            }
+         
+           req.files.map(x=> images.push(x.path));
+            res.send(images);
         });
-    });
-    return;
-
-    var img = fs.readFileSync(req.file.path);
-    var encode_image = img.toString('base64');
-    // Define a JSONobject for the image attributes for saving to database
-
-    var finalImg = {
-      contentType: req.file.mimetype,
-      image: new Buffer(encode_image, 'base64')
-    };
-
-    console.log(img);
-
-    const { accessToken } = req.body;
-
-    const parseToken = deCodeTokenForUser(accessToken);
-    if (parseToken) {
-      this.responseSuccess(res, parseToken);
-    } else {
-      return this.responseError(
-        res,
-        {
-          authenticated: false,
-          message: "token incorrect",
-        },
-        400
-      );
-    }
+     }
+     //else {
+    //   return this.responseError(
+    //     res,
+    //     {
+    //       authenticated: false,
+    //       message: "token incorrect",
+    //     },
+    //     400
+    //   );
+    // }
   }
+  async getFile(req, res){
+    const { name } = req.params.name;
+    res.sendFile(__dirname + `./uploads/${name}`);
+  };
+
+
 
 }
 
